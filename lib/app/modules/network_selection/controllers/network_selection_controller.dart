eb8a3becb16ab1e5777213ab2556f8c8a754ae53
DanhDue ExOICTIF <danhdue@gmail.com>
2025-12-22 11:23:22 +0700

    handle business logic for the network selection bottom sheet.

--- lib/app/modules/network_selection/controllers/network_selection_controller.dart
+++ lib/app/modules/network_selection/controllers/network_selection_controller.dart
@@ -1,24 +1,120 @@
 // Copyright (c) 2025, one of DanhDue ExOICTIF projects. All rights reserved.
 
 import 'package:d3_wallet/base/base_controller.dart';
+import 'package:d3_wallet/data/bean/response/network_object/network_object.dart';
+import 'package:d3_wallet/generated/locales.g.dart';
+import 'package:d3_wallet/utils/constants.dart';
+import 'package:d3_wallet/utils/extensions/string_ext.dart';
+import 'package:dart_helper_utils/dart_helper_utils.dart';
 import 'package:fimber/fimber.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_utils/src/extensions/internacionalization.dart';
 
 class NetworkSelectionController extends BaseController {
+  final selectedNetwork =
+      NetworkObject(id: NetworkIds.ALL, name: LocaleKeys.allNetworks.tr, logo: "").obs;
+  final lstNetworks = <NetworkObject>[].obs;
+  final RxList<NetworkObject> filteredNetworks = <NetworkObject>[].obs;
+  late TextEditingController? filterTextEditingController;
+  late FocusNode? filterTextFocusNode;
+  final filterTextIsFocus = false.obs;
+  final showFilterTextClearIcon = false.obs;
+  String? networkName;
+
   @override
   void onInit() {
     super.onInit();
     Fimber.d("onInit()");
+    filterTextEditingController = TextEditingController();
+    filterTextFocusNode = FocusNode();
+    lstNetworks.addAll([
+      NetworkObject(id: NetworkIds.ALL, name: LocaleKeys.allNetworks.tr, logo: ""),
+      NetworkObject(
+        id: NetworkIds.SOLANA,
+        name: "Solana Mainnet Beta",
+        logo: "https://s2.coinmarketcap.com/static/img/coins/200x200/5426.png",
+      ),
+    ]);
+    filteredNetworks.addAll(lstNetworks);
   }
 
   @override
   void onReady() {
     super.onReady();
     Fimber.d("onReady()");
+    _handleTokenNameTextFieldFocus();
   }
 
   @override
   void onClose() {
     super.onClose();
     Fimber.d("onClose()");
+    filterTextEditingController?.dispose();
+    filterTextFocusNode?.dispose();
+  }
+
+  _handleTokenNameTextFieldFocus() {
+    Fimber.d("_handleTokenNameTextFieldFocus");
+    filterTextFocusNode?.addListener(() {
+      if (filterTextFocusNode?.hasFocus == true) {
+        filterTextIsFocus.value = true;
+        if (networkName?.isNotEmpty == true) showFilterTextClearIcon.value = true;
+      } else {
+        _validateNetworkName();
+        filterTextIsFocus.value = false;
+        if (networkName?.isNotEmpty == true) {
+          showFilterTextClearIcon.value = true;
+        } else {
+          showFilterTextClearIcon.value = false;
+        }
+      }
+    });
+  }
+
+  Future<void> _validateNetworkName() async {
+    Fimber.d("_validateFirst()");
+  }
+
+  void setInputs(NetworkObject? selectedNetwork) {
+    Fimber.d("setInputs(selectedNetwork: ${selectedNetwork?.toJson().encodedJsonString})");
+    this.selectedNetwork.value =
+        ((StringExt(selectedNetwork?.name)?.isNotBlank() == true)
+            ? selectedNetwork
+            : NetworkObject(id: NetworkIds.ALL, name: LocaleKeys.allNetworks.tr, logo: ""))!;
+  }
+
+  void updateSelectedNetwork(NetworkObject network) {
+    selectedNetwork.value = network;
+  }
+
+  void filterNetworksByName(String query) async {
+    if (query.isEmpty) {
+      filteredNetworks.value = lstNetworks;
+      return;
+    }
+    networkName = query;
+    showFilterTextClearIcon.value = true;
+
+    filteredNetworks.value =
+        lstNetworks
+            .where((network) => network.name?.toLowerCase().contains(query.toLowerCase()) == true)
+            .toList();
+  }
+
+  void clearFilterredText() {
+    Fimber.d("clearFilterredText()");
+    filteredNetworks.value = lstNetworks;
+    filterTextEditingController?.text = "";
+    networkName = '';
+    showFilterTextClearIcon.value = false;
+  }
+
+  void reset() {
+    Fimber.d("reset()");
+    filteredNetworks.value = lstNetworks;
+    filterTextEditingController?.text = "";
+    networkName = null;
+    showFilterTextClearIcon.value = false;
   }
 }
--- lib/app/modules/network_selection/views/network_selection_view.dart
+++ lib/app/modules/network_selection/views/network_selection_view.dart
@@ -2,9 +2,18 @@
 
 // coverage:ignore-file
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:d3_wallet/data/bean/response/network_object/network_object.dart';
+import 'package:d3_wallet/generated/assets.gen.dart';
+import 'package:d3_wallet/generated/locales.g.dart';
+import 'package:d3_wallet/styles/app_themes.dart';
+import 'package:d3_wallet/utils/constants.dart';
+import 'package:fimber/fimber.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:material_text_fields/material_text_fields.dart';
+import 'package:material_text_fields/theme/material_text_field_theme.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 import '../controllers/network_selection_controller.dart';
 
@@ -19,12 +28,261 @@ class NetworkSelectionView extends StatefulWidget {
 
 class _NetworkSelectionViewState extends State<NetworkSelectionView> {
   final controller = Get.put(NetworkSelectionController(), permanent: false);
+
+  @override
+  void initState() {
+    super.initState();
+    Fimber.d("initState()");
+    controller.setInputs(widget.selectedNetwork);
+  }
+
+  @override
+  void dispose() {
+    controller.reset();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('NetworkSelectionView'), centerTitle: true),
-      body: const Center(
-        child: Text('NetworkSelectionView is working', style: TextStyle(fontSize: 20)),
+    return GestureDetector(
+      onTap: () {
+        // call this method here to hide soft keyboard
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: GetBuilder(
+        init: controller,
+        builder: (controller) {
+          return CupertinoScaffold(
+            transitionBackgroundColor: Colors.transparent,
+            body: Container(
+              color: context.appThemes.white,
+              child: Wrap(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            InkWell(
+                              onTap: () => Get.back(),
+                              child: Container(
+                                width: 36,
+                                height: 36,
+                                padding: EdgeInsets.all(6),
+                                child: Assets.images.icCloseRound.svg(fit: BoxFit.contain),
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                LocaleKeys.selectNetwork.tr,
+                                style: context.appThemes.medium16.copyWith(
+                                  color: context.appThemes.ink100,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Visibility(
+                              visible: false,
+                              maintainSize: true,
+                              maintainState: true,
+                              maintainAnimation: true,
+                              child: Assets.images.icCloseRound.svg(
+                                width: 36,
+                                height: 36,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Divider(height: 1, color: context.appThemes.ink5),
+                      Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(height: 16),
+                            Obx(
+                              () => Focus(
+                                onFocusChange: (hasFocus) {
+                                  controller.filterTextIsFocus.value = hasFocus;
+                                },
+                                child: MaterialTextField(
+                                  style: context.appThemes.regular14,
+                                  keyboardType: TextInputType.text,
+                                  hint: LocaleKeys.search.tr,
+                                  labelText: LocaleKeys.search.tr,
+                                  textInputAction: TextInputAction.next,
+                                  prefixIcon: Container(
+                                    padding: EdgeInsetsGeometry.all(10),
+                                    child: Icon(
+                                      Icons.search_outlined,
+                                      size: 32,
+                                      color:
+                                          controller.filterTextIsFocus.value
+                                              ? context.appThemes.trueBlue
+                                              : context.appThemes.ink40,
+                                    ),
+                                  ),
+                                  suffixIcon:
+                                      controller.showFilterTextClearIcon.value
+                                          ? InkWell(
+                                            onTap: () => controller.clearFilterredText(),
+                                            child: Container(
+                                              padding: EdgeInsetsGeometry.all(10),
+                                              child: Assets.images.icClear.svg(
+                                                width: 10,
+                                                height: 10,
+                                                fit: BoxFit.cover,
+                                                colorFilter: ColorFilter.mode(
+                                                  context.appThemes.ink60,
+                                                  BlendMode.srcATop,
+                                                ),
+                                              ),
+                                            ),
+                                          )
+                                          : SizedBox.shrink(),
+                                  theme: FilledOrOutlinedTextTheme(
+                                    radius: 6,
+                                    contentPadding: const EdgeInsets.symmetric(
+                                      horizontal: 16,
+                                      vertical: 10,
+                                    ),
+                                    errorStyle: context.appThemes.regular14.copyWith(
+                                      color: context.appThemes.red,
+                                    ),
+                                    fillColor: Colors.transparent,
+                                    prefixIconColor:
+                                        controller.filterTextIsFocus.value
+                                            ? context.appThemes.trueBlue
+                                            : context.appThemes.ink40,
+                                    enabledColor: context.appThemes.ink10,
+                                    focusedColor: context.appThemes.trueBlue,
+                                    floatingLabelStyle: TextStyle(
+                                      color: context.appThemes.trueBlue,
+                                    ),
+                                    labelStyle: context.appThemes.regular10.copyWith(
+                                      color: context.appThemes.ink40,
+                                    ),
+                                    iconColor:
+                                        controller.filterTextIsFocus.value
+                                            ? context.appThemes.trueBlue
+                                            : context.appThemes.ink40,
+                                  ),
+                                  controller: controller.filterTextEditingController,
+                                  onChanged: (value) => controller.filterNetworksByName(value),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 232,
+                        child: Obx(
+                          () =>
+                              !controller.filteredNetworks.isNotEmpty
+                                  ? _buildNotFoundItem(context)
+                                  : ListView(
+                                    children: [
+                                      for (final network in controller.filteredNetworks)
+                                        _buildNetworkItem(context, network, controller),
+                                    ],
+                                  ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  _buildNotFoundItem(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(16),
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: Text(
+          LocaleKeys.searchingNotFound.tr,
+          style: context.appThemes.regular14.copyWith(color: context.appThemes.ink100),
+        ),
+      ),
+    );
+  }
+
+  _buildNetworkItem(
+    BuildContext context,
+    NetworkObject network,
+    NetworkSelectionController controller,
+  ) {
+    return InkWell(
+      onTap: () {
+        controller.updateSelectedNetwork(network);
+        Future.delayed(Duration(milliseconds: Constants.keyboardDismissDuration), () {
+          Get.back(result: network);
+        });
+      },
+      child: Obx(
+        () => Container(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          color:
+              controller.selectedNetwork.value.name == network.name
+                  ? context.appThemes.blue0
+                  : context.appThemes.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              if (network.id == NetworkIds.ALL)
+                SizedBox(
+                  width: 48,
+                  height: 48,
+                  child: Icon(
+                    Icons.connected_tv_outlined,
+                    size: 36,
+                    color: context.appThemes.trueBlue,
+                  ),
+                )
+              else
+                Container(
+                  width: 48,
+                  height: 48,
+                  padding: EdgeInsets.all(6),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: CachedNetworkImage(
+                      imageUrl: network.logo ?? "",
+                      width: 36,
+                      height: 36,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              SizedBox(width: 16),
+              Text(
+                network.name ?? "",
+                style: context.appThemes.medium16.copyWith(color: context.appThemes.ink100),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
